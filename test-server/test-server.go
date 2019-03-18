91441422ef88c7fb0a8a1eddc888e060621a2a72
Sergey Soldatov <ssacompany@mail.ru>
2019-03-19 00:22:40 +0300

    fix exit from app

--- test-server/test-server.go
+++ test-server/test-server.go
@@ -23,21 +23,6 @@ var (
 	input_reg_cnt       = flag.Int("input_reg_cnt", 100, "input register counter")
 )
 
-func getFireSignalsChannel() chan os.Signal {
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c,
-		// https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
-		syscall.SIGTERM, // "the normal way to politely ask a program to terminate"
-		syscall.SIGINT,  // Ctrl+C
-		syscall.SIGQUIT, // Ctrl-\
-		syscall.SIGKILL, // "always fatal", "SIGKILL and SIGSTOP may not be caught by a program"
-		syscall.SIGHUP,  // "terminal is disconnected"
-	)
-	return c
-
-}
-
 func main() {
 	var err error
 
@@ -51,19 +36,24 @@ func main() {
 		modbus.StringToModbusTypeProtocol(*mbprotocol),
 		*coils_cnt, *discrete_inputs_cnt, *holding_reg_cnt, *input_reg_cnt)
 
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func(s *modbus.ModbusServer) {
-		<-c
+	// Exit handler
+	exit := make(chan struct{})
+	closeSignal := make(chan os.Signal)
+	signal.Notify(closeSignal, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-closeSignal
+		srv.Stop()
 		fmt.Println("\nExit program")
-		s.ServerStop()
-		os.Exit(1)
-	}(srv)
+		close(exit)
+	}()
 
 	srv.Data.PresetMultipleRegisters(0, 5, []uint16{0x01, 0x02, 0x03, 0x04, 0x05})
-	err = srv.ServerStart()
+	err = srv.Start()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	// Exit app if chan is closed
+	<-exit
 }
