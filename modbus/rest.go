ffd77e184f47fc93a4ebfce16740a188128aac5d
Sergey Soldatov <ssacompany@mail.ru>
2019-03-19 23:21:02 +0300

    begin create rest service for modbus

---
+++ modbus/rest.go
@@ -0,0 +1,28 @@
+// rest
+package modbus
+
+/*
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type ModbusRESTreq struct {
+	id   int `json:"id"`
+	fc   int `json:"fc"`
+	addr int `json:"addr"`
+	cnt  int `json:"cnt"`
+}
+
+func CreateRest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/read", readModbusData).Methods("GET")
+	r.HandleFunc("/books", writeModbusData).Methods("POST")
+	http.ListenAndServe(":8000", r)
+}
+*/
