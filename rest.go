cdec8148cb6b66ccb723d4943d664f6c575bcc69
Sergey Soldatov <ssacompany@mail.ru>
2019-03-24 14:27:50 +0300

    Fully support rest-service

--- rest.go
+++ rest.go
@@ -6,61 +6,206 @@ package modbus
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"strconv"
 )
 
-var md ModbusData
-
-type ModbusReq struct {
-	addr uint16 `json:"addr"`
-	cnt  uint16 `json:"cnt"`
+// ModbusServer implements server interface
+type ModbusRest struct {
+	ModbusBase                // Anonim ModbusBase implementation
+	Router     *http.ServeMux // HTTP request multiplexer
 }
 
+// Rest answer for Holding/Input registers request
 type ModbusRegAnswer struct {
-	data []uint16 `json:"data"`
+	Data []uint16 `json:"data"` // Read values
 }
 
+// Rest answer for Coils/DigitInputs
 type ModbusBoolAnswer struct {
-	data []bool `json:"data"`
+	Data []bool `json:"data"` // Read values
 }
 
+// Rest request to write Holding/Input registers
 type ModbusWriteRegReq struct {
-	addr int      `json:"addr"`
-	data []uint16 `json:"data"`
+	Addr uint16   `json:"addr"` // Addres first element
+	Data []uint16 `json:"data"` // Values for writing
 }
 
+// Rest request to write Coils/DigitInputs
 type ModbusWriteBoolReq struct {
-	addr uint16 `json:"addr"`
-	data []bool `json:"data"`
+	Addr uint16 `json:"addr"` // Addres first element
+	Data []bool `json:"data"` // Values for writing
 }
 
-func readHoldReg(w http.ResponseWriter, r *http.Request) {
+// Return string with host ip/name and port
+func (rest *ModbusRest) String() string {
+	return rest.Host + ":" + rest.Port
+}
+
+// Build a response to an unknown request
+func errAnswer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GO AWAY", r.Method, r.URL.Path)
+}
+
+// Handler for GET/PUT request Coils
+func (rest *ModbusRest) hndlCoils(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	var (
-		req    ModbusReq
+		answer ModbusBoolAnswer
+		err    error
+		addr   int
+		cnt    int
+	)
+
+	switch r.Method {
+	case "POST":
+		var req ModbusWriteBoolReq
+		_ = json.NewDecoder(r.Body).Decode(&req)
+		rest.Data.ForceMultipleCoils(req.Addr, req.Data)
+
+		answer.Data, err = rest.Data.ReadCoilStatus(req.Addr, uint16(len(req.Data)))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+	case "GET":
+		query := r.URL.Query()
+		addr, err = strconv.Atoi(query.Get("addr"))
+		cnt, err = strconv.Atoi(query.Get("cnt"))
+
+		answer.Data, err = rest.Data.ReadCoilStatus(uint16(addr), uint16(cnt))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+
+	default:
+		errAnswer(w, r)
+	}
+}
+
+// Handler for GET request DigitInputs
+func (rest *ModbusRest) hndlDigitInputs(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var (
+		answer ModbusBoolAnswer
+		err    error
+		addr   int
+		cnt    int
+	)
+
+	switch r.Method {
+	case "GET":
+		query := r.URL.Query()
+		addr, err = strconv.Atoi(query.Get("addr"))
+		cnt, err = strconv.Atoi(query.Get("cnt"))
+
+		answer.Data, err = rest.Data.ReadDescreteInputs(uint16(addr), uint16(cnt))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+
+	default:
+		errAnswer(w, r)
+	}
+
+}
+
+// Handler for GET/PUT request Holding Registers
+func (rest *ModbusRest) hndlHoldReg(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var (
 		answer ModbusRegAnswer
 		err    error
+		addr   int
+		cnt    int
 	)
-	_ = json.NewDecoder(r.Body).Decode(&req)
-	answer.data, err = md.ReadHoldingRegisters(req.addr, req.cnt)
-	if err != nil {
-		return
+
+	switch r.Method {
+	case "POST":
+		var req ModbusWriteRegReq
+		_ = json.NewDecoder(r.Body).Decode(&req)
+		rest.Data.PresetMultipleRegisters(req.Addr, req.Data)
+
+		answer.Data, err = rest.Data.ReadHoldingRegisters(req.Addr, uint16(len(req.Data)))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+	case "GET":
+		query := r.URL.Query()
+		addr, err = strconv.Atoi(query.Get("addr"))
+		cnt, err = strconv.Atoi(query.Get("cnt"))
+
+		answer.Data, err = rest.Data.ReadHoldingRegisters(uint16(addr), uint16(cnt))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+
+	default:
+		errAnswer(w, r)
 	}
-	json.NewEncoder(w).Encode(answer)
 }
 
-func CreateRest() {
-	r := mux.NewRouter()
+// Handler for GE request Inputs Registers
+func (rest *ModbusRest) hndlInputReg(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-	/*	r.HandleFunc("/coils", readModbusData).Methods("GET")
-		r.HandleFunc("/coils", readModbusData).Methods("POST")
-		r.HandleFunc("/d_in", writeModbusData).Methods("GET")
-		r.HandleFunc("/d_in", writeModbusData).Methods("POST")*/
-	r.HandleFunc("/hold_reg", readHoldReg).Methods("GET")
-	//	r.HandleFunc("/hold_reg", writeHoldReg).Methods("POST")
-	/*	r.HandleFunc("/in_reg", writeModbusData).Methods("GET")
-		r.HandleFunc("/in_reg", writeModbusData).Methods("POST")*/
-	http.ListenAndServe(":8000", r)
+	var (
+		answer ModbusRegAnswer
+		err    error
+		addr   int
+		cnt    int
+	)
+
+	switch r.Method {
+	case "GET":
+		query := r.URL.Query()
+		addr, err = strconv.Atoi(query.Get("addr"))
+		cnt, err = strconv.Atoi(query.Get("cnt"))
+
+		answer.Data, err = rest.Data.ReadInputRegisters(uint16(addr), uint16(cnt))
+		if err != nil {
+			return
+		}
+		json.NewEncoder(w).Encode(answer.Data)
+
+	default:
+		errAnswer(w, r)
+	}
+}
+
+// Create new Rest-server for Modbus Data
+func NewRest(host, port string, md *ModbusData) *ModbusRest {
+	rest := new(ModbusRest)
+	rest.Host = host
+	rest.Port = port
+	rest.Data = md
+	rest.Router = http.NewServeMux()
+
+	rest.Router.HandleFunc("/coils", rest.hndlCoils)
+	rest.Router.HandleFunc("/d_in", rest.hndlDigitInputs)
+	rest.Router.HandleFunc("/hold_reg", rest.hndlHoldReg)
+	rest.Router.HandleFunc("/in_reg", rest.hndlInputReg)
+
+	return rest
+}
+
+// Start Rest-server for Modbus Data
+func (rest *ModbusRest) Start() error {
+	var err error
+	fmt.Println("Server startup...")
+	fmt.Println("Listening at", rest.String())
+
+	go func() {
+		err = http.ListenAndServe(rest.String(), rest.Router)
+	}()
+	return err
 }
