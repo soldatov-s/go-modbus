815ed4bcac1836117cb7e5a953de8a6c13eb1d81
Sergey Soldatov <ssacompany@mail.ru>
2019-03-26 01:25:42 +0300

    changed package name

--- modbusgrpc/grpcClient.go
+++ modbusgrpc/grpcClient.go
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache license. See the LICENSE file for details.
 
-package modbus
+package modbusgrpc
 
 import (
 	"encoding/json"
