4ba911f6ddee33e13a02c852eacee8d994cc688c
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-27 16:44:14 +0300

    Create base_test.go

---
+++ base_test.go
@@ -0,0 +1,30 @@
+// Copyright 2019 Sergey Soldatov. All rights reserved.
+// This software may be modified and distributed under the terms
+// of the Apache license. See the LICENSE file for details.
+
+package modbus
+
+import (
+	"testing"
+  "strings"
+)
+
+func TestModbusBaseServer_String(t *testing.T) {
+  test_str := "test:504"
+  srv := &ModbusBaseServer{Host: "test", Port: "504"}
+  s := fmt.Sprintf("%s", srv)
+  r := strings.Compare(s, test_str)
+  if r != 0 {
+		t.Error("Expected ", test_str, "got ",r)
+  }
+}
+
+func TestModbusBaseClient_String(t *testing.T) {
+  test_str := "test:504"
+  srv := &ModbusBaseServer{Host: "test", Port: "504"}
+  s := fmt.Sprintf("%s", srv)
+  r := strings.Compare(s, test_str)
+  if r != 0 {
+		t.Error("Expected ", test_str, "got ",r)
+  }
+}
