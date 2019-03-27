083202fb7d62c857474f2bc3f750b30850d6ce1d
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-27 11:59:41 +0300

    Update packet.go

--- packet.go
+++ packet.go
@@ -76,6 +76,7 @@ func (mp *ModbusPacket) GetCrc() uint16 {
 	if mp.Length == 0 || mp.TypeProtocol == ModbusTCP {
 		return 0
 	}
+	fmt.Println(mp.Data[mp.Length-2 : mp.Length])
 	return binary.BigEndian.Uint16(mp.Data[mp.Length-2 : mp.Length])
 }
 
