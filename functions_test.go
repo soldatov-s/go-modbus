a02928c9f09bee21f0cb02697ff9a38fc5dd9b2e
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-28 10:49:03 +0300

    Create functions_test.go

---
+++ functions_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Sergey Soldatov. All rights reserved.
+// This software may be modified and distributed under the terms
+// of the Apache license. See the LICENSE file for details.
+
+package modbus
