73e2456ce6a7e1284be39f316a5d659a057dff00
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-28 17:11:29 +0300

    clearned code

--- modbusgrpc/grpcClient.go
+++ modbusgrpc/grpcClient.go
@@ -22,100 +22,59 @@ type ModbusgRPCClient struct {
 
 func NewgRPCClient(port, host string) *ModbusgRPCClient {
 	var err error
-	cl := new(ModbusgRPCClient)
-	cl.Port = port
-	cl.Host = host
-
+	cl := &ModbusgRPCClient{Port: port, Host: host}
 	cl.Conn, err = grpc.Dial(cl.String(), grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Can not connected: %v", err)
 	}
-
 	cl.ServiceClient = NewModbusServiceClient(cl.Conn)
-
 	return cl
 }
 
-func (cl *ModbusgRPCClient) ReadHoldingRegisters(addr, cnt int32) []int32 {
-	var err error
-
-	request := &ModbusRequest{Addr: addr, Cnt: cnt}
-
-	answer, err := cl.ServiceClient.ReadHoldingRegisters(context.Background(), request)
+func handelErr(err error, fmsg string) {
 	if err != nil {
-		log.Fatalf("Can't read holding registers: %v", err)
+		log.Fatalf(fmsg, err)
 	}
+}
 
-	log.Println(answer.Data)
+func (cl *ModbusgRPCClient) ReadHoldingRegisters(addr, cnt int32) []int32 {
+	request := &ModbusRequest{Addr: addr, Cnt: cnt}
+	answer, err := cl.ServiceClient.ReadHoldingRegisters(context.Background(), request)
+	handelErr(err, "Can't read holding registers: %v")
 	return answer.Data
 }
 
 func (cl *ModbusgRPCClient) ReadInputRegisters(addr, cnt int32) []int32 {
-	var err error
-
 	request := &ModbusRequest{Addr: addr, Cnt: cnt}
-
 	answer, err := cl.ServiceClient.ReadInputRegisters(context.Background(), request)
-	if err != nil {
-		log.Fatalf("Can't read input registers: %v", err)
-	}
-
-	log.Println(answer.Data)
+	handelErr(err, "Can't read input registers: %v")
 	return answer.Data
 }
 
 func (cl *ModbusgRPCClient) ReadCoilStatus(addr, cnt int32) []bool {
-	var err error
-
 	request := &ModbusRequest{Addr: addr, Cnt: cnt}
-
 	answer, err := cl.ServiceClient.ReadCoilStatus(context.Background(), request)
-	if err != nil {
-		log.Fatalf("Can't read coils: %v", err)
-	}
-
-	log.Println(answer.Data)
+	handelErr(err, "Can't read coils: %v")
 	return answer.Data
 }
 
 func (cl *ModbusgRPCClient) ReadDescreteInputs(addr, cnt int32) []bool {
-	var err error
-
 	request := &ModbusRequest{Addr: addr, Cnt: cnt}
-
 	answer, err := cl.ServiceClient.ReadDescreteInputs(context.Background(), request)
-	if err != nil {
-		log.Fatalf("Can't read inputs: %v", err)
-	}
-
-	log.Println(answer.Data)
+	handelErr(err, "Can't read inputs: %v")
 	return answer.Data
 }
 
 func (cl *ModbusgRPCClient) PresetMultipleRegisters(addr int32, data []int32) []int32 {
-	var err error
-
 	request := &ModbusWriteRegistersRequest{Addr: addr, Data: data}
-
 	answer, err := cl.ServiceClient.PresetMultipleRegisters(context.Background(), request)
-	if err != nil {
-		log.Fatalf("Can't write holding registers: %v", err)
-	}
-
-	log.Println(answer.Data)
+	handelErr(err, "Can't write holding registers: %v")
 	return answer.Data
 }
 
 func (cl *ModbusgRPCClient) ForceMultipleCoils(addr int32, data []bool) []bool {
-	var err error
-
 	request := &ModbusWriteBitsRequest{Addr: addr, Data: data}
-
 	answer, err := cl.ServiceClient.ForceMultipleCoils(context.Background(), request)
-	if err != nil {
-		log.Fatalf("Can't write coils: %v", err)
-	}
-
-	log.Println(answer.Data)
+	handelErr(err, "Can't write coils: %v")
 	return answer.Data
 }
