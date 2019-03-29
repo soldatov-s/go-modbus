e48406d1b9cefc5c57a3a63438b10967732085f5
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-29 16:03:57 +0300

    added new tests

--- packet_test.go
+++ packet_test.go
@@ -38,9 +38,36 @@ func TestModbusPacket_Crc16Check(t *testing.T) {
 	}
 }
 
+func TestModbusPacket_GetAddr(t *testing.T) {
+	test_data := []byte{0x1, 0x3, 0x0, 0x0, 0x0, 0xA, 0xCD, 0xC5}
+	mp := &ModbusPacket{TypeProtocol: ModbusRTUviaTCP,
+		Data:   test_data,
+		Length: len(test_data)}
+	res := mp.GetAddr()
+	if res != byte(0x1) {
+		t.Error("Expected", byte(0x1), "got", res)
+	}
+	test_data := []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x3, 0x0, 0x0, 0x0, 0xA}
+	mp := &ModbusPacket{TypeProtocol: ModbusTCP,
+		Data:   test_data,
+		Length: len(test_data)}
+	res := mp.GetAddr()
+	if res != byte(0x1) {
+		t.Error("Expected", byte(0x1), "got", res)
+	}
+}
+
 func TestModbusPacket_GetFC(t *testing.T) {
 	test_data := []byte{0x1, 0x3, 0x0, 0x0, 0x0, 0xA, 0xCD, 0xC5}
 	mp := &ModbusPacket{TypeProtocol: ModbusRTUviaTCP,
+		Data:   test_data,
+		Length: len(test_data)}
+	res := mp.GetFC()
+	if res != ReadHoldingRegisters {
+		t.Error("Expected", ReadHoldingRegisters, "got", res)
+	}
+	test_data := []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x3, 0x0, 0x0, 0x0, 0xA}
+	mp := &ModbusPacket{TypeProtocol: ModbusTCP,
 		Data:   test_data,
 		Length: len(test_data)}
 	res := mp.GetFC()
@@ -57,5 +84,13 @@ func TestModbusPacket_GetCrc(t *testing.T) {
 	res := mp.GetCrc()
 	if res != uint16(0xCDC5) {
 		t.Error("Expected", uint16(0xCDC5), "got", res)
+	}
+	test_data := []byte{0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x3, 0x0, 0x0, 0x0, 0xA}
+	mp := &ModbusPacket{TypeProtocol: ModbusTCP,
+		Data:   test_data,
+		Length: len(test_data)}
+	res := mp.GetFC()
+	if res != uint16(0x0) {
+		t.Error("Expected", uint16(0x0, "got", res)
 	}
 }
