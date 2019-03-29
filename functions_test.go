2046464c04849d2a8ac4c1098f37cdfc94373c30
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-29 12:39:33 +0300

    Added new test

--- functions_test.go
+++ functions_test.go
@@ -11,6 +11,109 @@ import (
 	"testing"
 )
 
+func TestboolCntToByteCnt(t *testing.T) {
+	test_data := 9
+	res := boolCntToByteCnt(test_data)
+	v := 2
+	if v != res {
+		t.Error("Expected ", v, "got ", res)
+	}
+}
+
+func TestboolArrToByteArr(t *testing.T) {
+	test_data := []bool{true, true, false, false, true}
+	res := boolArrToByteArr(test_data)
+	v := 19
+	if v != res {
+		t.Error("Expected ", v, "got ", res)
+	}
+}
+
+func TestwordArrToByteArr(t *testing.T) {
+	test_data := []uint16{0x01, 0x02, 0x03, 0x04}
+	res := wordArrToByteArr(test_data)
+	for i, v := range test_data {
+		t := binary.BigEndian.Uint16(res[2*i : 2+2*i])
+		if v != t {
+			t.Error("Expected ", v, "got ", t)
+		}
+	}
+}
+
+func TestbyteArrToBoolArr(t *testing.T) {
+	test_data := []byte{0x19}
+	test_cnt := uint16(5)
+	target := []bool{true, true, false, false, true}
+	res := byteArrToBoolArr(test_data, test_cnt)
+	for i, v := range target {
+		if v != res[i] {
+			t.Error("Expected ", v, "got ", res[i])
+		}
+	}
+}
+
+func TestbyteArrToWordArr(t *testing.T) {
+	test_data := []byte{0x0, 0x01, 0x0, 0x02, 0x0, 0x03, 0x0, 0x04}
+	res := boolArrToByteArr(test_data)
+	for i, v := range res {
+		t := binary.BigEndian.Uint16(test_data[2*i : 2+2*i])
+		if v != t {
+			t.Error("Expected ", t, "got ", v)
+		}
+	}
+}
+
+type testbuildPacketpair struct {
+	isReq        bool
+	TypeProtocol ModbusTypeProtocol
+	dev_id 	     byte
+	fc           ModbusFunctionCode
+	par1, par2   uint16
+	data         []byte
+}
+
+var testsbuildPacket = []testbuildPacketpair{
+	{true, ModbusRTUviaTCP,  1, ReadHoldingRegisters, 0x0, 0xA, []byte{0x1, 0x3, 0x0, 0x0, 0x0, 0xA, 0xCD, 0xC5}},
+}
+
+func TestbuildPacket(t *testing.T) {
+	for _, pair := range testsbuildPacket {
+		mp := buildPacket(pair.isReq, pair.TypeProtocol, pair.dev_id, pair.fc, pair.par1, pair.par2)
+		for i, v := range mp.Data {
+			if v != pair.data[i] {
+				t.Error(
+					"For", pair,
+					"expected", pair.data[i],
+					"got", v,
+				)
+			}
+		}
+	}
+}
+
+type testFunctionCodeLengthpair struct {
+	fc     ModbusFunctionCode
+	isReq  bool
+	length int
+}
+
+var testsFunctionCodeLength = []testFunctionCodeLengthpair{
+	{ReadCoilStatus, true, 4},
+}
+
+func TestModbusFunctionCode_Length(t *testing.T) {
+	for _, pair := range testsFunctionCodeLength {
+		res := pair.fc.Length(pair.isReq)
+		if res != pair.length {
+			t.Error(
+				"For", pair,
+				"expected", pair.length,
+				"got", res,
+			)
+		}
+	}
+}
+
 func TestReadHoldingRegistersHndl(t *testing.T) {
 	test_data := []uint16{0x01, 0x02, 0x03, 0x04, 0x05}
 	md := &ModbusData{holding_reg: make([]uint16, 10), mu_holding_regs: &sync.Mutex{}}
