69eaf6e49d53a8a8feb04a26c0f686f2267c16aa
Sergey Soldatov <46220264+soldatov-s@users.noreply.github.com>
2019-03-29 16:14:44 +0300

    Added new tests

--- functions_test.go
+++ functions_test.go
@@ -194,3 +194,25 @@ func TestReadDescreteInputsHndl(t *testing.T) {
 		}
 	}
 }
+
+func TestPresetSingleRegisterHndl(t *testing.T) {
+	test_data := uint16(0x01)
+	test_addr := uint16(0x0)
+	md := &ModbusData{holding_reg: make([]uint16, 10), mu_holding_regs: &sync.Mutex{}}
+	req := buildPacket(true, ModbusRTUviaTCP, 1, PresetSingleRegister, test_addr, test_data)
+	PresetSingleRegister(req, md)
+	if md.holding_reg[test_addr] != test_data {
+		t.Error("Expected ", test_data, "got ", md.holding_reg[test_addr])
+	}
+}
+
+func TestForceSingleCoilHndl(t *testing.T) {
+	test_data := uint16(0x01)
+	test_addr := uint16(0xFF00)
+	md := &ModbusData{coils: make([]bool, 10), mu_coils: &sync.Mutex{}}
+	req := buildPacket(true, ModbusRTUviaTCP, 1, ForceSingleCoil, test_addr, test_data)
+	PresetSingleRegister(req, md)
+	if md.coils[test_addr] != true {
+		t.Error("Expected ", true, "got ", md.coils[test_addr])
+	}
+}
